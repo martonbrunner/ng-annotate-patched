3cf83e962f4cfcdaac3624a196f5049554d82586
Ran Benita <ran234@gmail.com>
2017-09-12 20:27:16 +0300

    Fix != -> !== in traverse.js

--- src/traverse.js
+++ src/traverse.js
@@ -3,17 +3,15 @@
 
 const walk = require("acorn/dist/walk");
 
-function traverse(rootNode, options) {
+module.exports = function traverse(rootNode, options) {
     const ancestors = [];
     (function c(node, st, override) {
         const parent = ancestors[ancestors.length - 1];
-        const isNew = node != parent;
+        const isNew = node !== parent;
         if (options.pre && isNew) options.pre(node, parent);
         if (isNew) ancestors.push(node);
         walk.base[override || node.type](node, st, c);
         if (isNew) ancestors.pop();
         if (options.post && isNew) options.post(node, parent);
     })(rootNode);
-}
-
-module.exports = traverse;
+};
