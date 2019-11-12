0129f919e2ae63bae5d9a27a8201b57eac3c9508
Márton Brunner <marton.brunner@blackbelt.hu>
2019-11-12 15:47:14 +0100

    Fix sourcemap generation regarding member expressions
    acorn-walk by default skips the property part of member expressions
    when iterating. This meant that the properties were in the same node
    as the base object when generating source maps.
    
    E.g. in "this.foo.bar = 42;" the left part would not have separate
    mapping entries, "this.foo.bar" would be mapped to the same node.

--- src/traverse.js
+++ src/traverse.js
@@ -3,6 +3,13 @@
 
 const walk = require("acorn-walk");
 
+const customWalkFunctions = walk.make({
+    MemberExpression: (node, st, c) => {
+        c(node.object, st, "Expression");
+        c(node.property, st, "Expression");
+    },
+});
+
 module.exports = function traverse(rootNode, options, pluginOptions = {}) {
     const ancestors = [];
     (function c(node, st, override) {
@@ -10,7 +17,7 @@ module.exports = function traverse(rootNode, options, pluginOptions = {}) {
         const isNew = node !== parent;
         if (options.pre && isNew) options.pre(node, parent);
         if (isNew) ancestors.push(node);
-        walk.base[override || node.type](node, st, c);
+        customWalkFunctions[override || node.type](node, st, c);
         if (isNew) ancestors.pop();
         if (options.post && isNew) options.post(node, parent);
     })(rootNode);
